bc172edfc77dec13b113e8404dc7881756a0a8fe
Pavel Naumovets <pavel.naumovets@mail.ru>
2021-05-31 20:40:40 +0300

    Answer

--- src/main/java/com/epam/rd/autotasks/segments/Segment.java
+++ src/main/java/com/epam/rd/autotasks/segments/Segment.java
@@ -25,18 +25,10 @@ class Segment {
     Point intersection(Segment another) {
         //если оба отрезка вертикальные
         if((start.getX() - end.getX() == 0) && (another.start.getX() - another.end.getX() == 0)) {
-
             //если они лежат на одном X
             if(start.getX() == another.start.getX()) {
-
-                //проверим пересекаются ли они, т.е. есть ли у них общий Y
-                //для этого возьмём отрицание от случая, когда они НЕ пересекаются
-                if (!((Math.max(start.getY(), end.getY()) < Math.min(another.start.getY(), another.end.getY())) ||
-                        (Math.min(start.getY(), end.getY()) > Math.max(another.start.getY(), another.end.getY())))) {
-                    return null;
-                }
+                return null;
             }
-
             return null;
         }
 
@@ -57,9 +49,9 @@ class Segment {
                     Math.max(start.getY(), end.getY()) >= Ya) {
 
                 return new Point(Xa,Ya);
+            } else{
+                return null;
             }
-
-            return null;
         }
 
         //если второй отрезок вертикальный
@@ -75,9 +67,9 @@ class Segment {
                     Math.max(another.start.getY(), another.end.getY()) >= Ya) {
 
                 return new Point(Xa,Ya);
+            }else{
+                return null;
             }
-
-            return null;
         }
 
         //оба отрезка невертикальные
@@ -85,20 +77,20 @@ class Segment {
         double A2 = (another.start.getY() - another.end.getY()) / (another.start.getX() - another.end.getX());
         double b1 = start.getY() - A1 * start.getX();
         double b2 = another.start.getY() - A2 * another.start.getX();
-
-        if (A1 == A2) {
-            return null; //отрезки параллельны
-        }
+        double delta = A1 * b2 - A2 * b1;
 
         //Xa - абсцисса точки пересечения двух прямых
         double Xa = (b2 - b1) / (A1 - A2);
         double Ya = A1 * Xa + b1;
 
-        if ((Xa < Math.max(start.getX(), another.start.getX())) || (Xa > Math.min( end.getX(), another.end.getX()))) {
-            return null; //точка Xa находится вне пересечения проекций отрезков на ось X
+        if (A1 == A2) {
+            return null;
         }
-        else {
+        else if(delta > 0){
             return new Point(Xa, Ya);
+        }
+        else{
+            return null;
         }
     }
 }
